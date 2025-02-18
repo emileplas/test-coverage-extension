63f91305da11f5657407b9c1abab7e8fca19d8d2
emileplas <39679292+emileplas@users.noreply.github.com>
2025-02-18 19:06:30 -0300

    Create line diff overview

--- core/src/main/java/com/brabel/coverage/extension/core/GitInteractor.java
+++ core/src/main/java/com/brabel/coverage/extension/core/GitInteractor.java
@@ -4,8 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GitInteractor {
 
@@ -73,5 +77,82 @@ public class GitInteractor {
 
         process.waitFor(); // Wait for the process to finish
         return output.toString().trim(); // Return the output as a string
+    }
+
+
+    public static HashMap<String, int[]> getChangedLines(String branchToCompare) throws IOException, InterruptedException {
+        // Prepare the git diff command to get detailed changes
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("git", "diff", branchToCompare);
+
+        // Start the process
+        Process process = processBuilder.start();
+
+        HashMap<String, int[]> changedLinesPerFile = new HashMap<>();
+
+        // Read the output
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            boolean passedFirstFileLine = false;
+            boolean passedFirstClassLine = false;
+            ArrayList<Integer> lines = null;
+            int lineIndex = -2;
+            String file = null;
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println(line);
+
+                if(isLineDiffGitLine(line)){
+                    file = line;
+                    if(passedFirstClassLine){
+                        changedLinesPerFile.put(file, lines.stream().mapToInt(Integer::intValue).toArray());
+                    }
+
+                    passedFirstFileLine = false;
+                    passedFirstClassLine = false;
+                    lines = new ArrayList<>();
+                    lineIndex = -2;
+                }
+
+                if(line.startsWith("@@") && line.endsWith("@@")){
+                    passedFirstFileLine = true;
+                    lineIndex = -1;
+                }
+
+                if(passedFirstFileLine){
+                    lineIndex++;
+                }
+
+                if(line.contains("class") && !passedFirstClassLine){
+                    passedFirstClassLine = true;
+                }
+
+                if(passedFirstClassLine){
+                    if(line.startsWith("+")){
+                        lines.add(lineIndex);
+                    }
+                }
+
+
+                //System.out.println(line); // Output the detailed changes
+            }
+        }
+
+        // Wait for the process to complete
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            System.err.println("Error executing git command: " + exitCode);
+        }
+
+        return changedLinesPerFile;
+    }
+
+    private static final String REGEX = "^diff --git a/.* b/.*$";
+
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+
+    protected static boolean isLineDiffGitLine(String line){
+        Matcher matcher = PATTERN.matcher(line);
+        return matcher.matches();
     }
 }
